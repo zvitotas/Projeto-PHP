f549fc7a9f6b2a2b3c3e898b439c92a844a391e3
Victor <vitotas2011@gmail.com>
2026-04-14 18:59:42 -0300

    Vi como utilizar regex no php realizei substituições utilizando regex. finalizei curso de strings no php. iniciando curso sobre manipular coleções com arrays, botando em pratica com funcionam os arrays.

---
+++ arrays.php
@@ -0,0 +1,13 @@
+<?php
+
+$array = [
+    1 => 'um',
+    true => 'dois',
+    1.9 => 'tres',
+    '1' => 'quatro',
+    false => 'cinco',
+    0 => 'seis',
+    null => 'sete'
+];
+
+var_dump($array);
--- regex.php
+++ regex.php
@@ -3,10 +3,23 @@
 $telefones = ['(54) 9922 - 7671', '(54) 9945 - 9168', '(54) 9810 - 5499'];
 
 foreach ($telefones as $telefone) {
-    $telefoneValido = preg_match('/^\([0-9]{2}\) 9?[0-9]{4} - [0-9]{4}$/', $telefone);
-    if($telefoneValido) {
+    $regex = '/^\(([0-9]{2})\) ([0-9]{4,5} - [0-9]{4})$/';
+
+    $telefoneValido = preg_match(
+        $regex,
+        $telefone,
+        $correspondencia
+    );
+
+    if ($telefoneValido) {
         echo 'Telefone válido' . PHP_EOL;
     } else {
         echo 'Telefone inválido' . PHP_EOL;
     }
+
+    echo preg_replace(
+        $regex,
+        '(XX) \2',
+        $telefone
+    ) . PHP_EOL;
 }
